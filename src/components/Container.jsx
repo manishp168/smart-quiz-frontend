37dfd6c774b8b55c210c71aa68e9ff0ce9252733
Manish Choudhary <manishpotliya1@gmail.com>
2025-04-12 13:13:44 +0530

    ScrollToTop component to reset scroll position on route change

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export const Container = ({children}) => {
+  const path = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    })
+  },[path])
   return (
     <div
     className='bg-[#eaeaea]  min-h-screen w-full overflow-x-hidden pt-20' style={{ scrollbarWidth: "none" }}
